5a5322249019e6c5f763e99883a217b762e9f5a1
asterno <113440405+asterno@users.noreply.github.com>
2023-05-01 11:11:45 +0300

    Forgor

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import  "./styles.module.css"
 import classes from "./styles.module.css"
-
+import {Link} from "react-router-dom"
  const Footer = () => {
   return (
     <div className={classes.FooterContainer}>
         <div className={classes.FooterText}>
-<p><Link className={classes.FooterLink}>о комнании</Link></p>
-    <p><Link className={classes.FooterLink}>услугии</Link></p>
-    <p><Link className={classes.FooterLink}>дизайнеры</Link></p>
+<p><Link className={classes.FooterLink} to="/OCompany"  >о комнании</Link></p>
+    <p><Link className={classes.FooterLink} to="/Uslugi">услугии</Link></p>
+    <p><Link className={classes.FooterLink} to="/Disayneri">дизайнеры</Link></p>
         </div>
     
    
